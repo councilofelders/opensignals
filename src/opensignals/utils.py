86910469d43719d95a379cc79d7ad2db47dd04ce
Jordi Villar <jordi.villar@kogniasports.com>
2021-07-14 08:57:26 +0200

    Add missing file

---
+++ src/opensignals/utils.py
@@ -0,0 +1,10 @@
+USER_AGENTS = [
+    (
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko)'
+        ' Chrome/39.0.2171.95 Safari/537.36'
+    ),
+    (
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)'
+        ' Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
+    )
+]
